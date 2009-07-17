9d603fb50cf32040fde1c21883dc2cd7c378005b
Justin Quick <justquick@gmail.com>
2009-07-17 11:35:41 -0400

    added simple setup.py so app can be used w/ pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "django-mptt-comments",
+    version = '0.1',
+    url = 'http://bitbucket.org/fivethreeo/django-mptt-comments/',
+    author = 'fivethreeo',
+    description = 'Django Mptt Comments is a simple way to display threaded comments instead of the django contrib comments.',
+    packages = ['mptt_comments']
+)
