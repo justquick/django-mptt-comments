1e84b63d7ab756a0c66bfea412e0674da8a713ff
Justin Quick <justquick@gmail.com>
2009-11-30 14:14:49 -0500

    updated admin to filter out root comments, comment text displayed in list

--- mptt_comments/admin.py
+++ mptt_comments/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf import settings
 from mptt_comments.models import MpttComment
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.comments.models import Comment
 
 class MpttCommentsAdmin(admin.ModelAdmin):
     fieldsets = (
@@ -16,10 +17,14 @@ class MpttCommentsAdmin(admin.ModelAdmin):
         )
      )
 
-    list_display = ('name', 'content_type', 'object_pk', 'ip_address', 'submit_date', 'is_public',   'level', 'lft', 'rght')
+    list_display = ('name', 'content_type', 'object_pk', 'ip_address', 'comment', 'submit_date', 'is_public')
     list_filter = ('submit_date', 'parent', 'site', 'is_public', 'is_removed')
     date_hierarchy = 'submit_date'
     ordering = ('-submit_date',)
     search_fields = ('comment', 'user__username', 'user_name', 'user_email', 'user_url', 'ip_address')
 
+    def queryset(self, request):
+        return MpttComment.objects.filter(parent__isnull=False)
+
+admin.site.unregister(Comment)
 admin.site.register(MpttComment, MpttCommentsAdmin)
