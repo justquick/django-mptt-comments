6d1bc9e2b302d5b6d86fa6d7b60f2ac00a7d2d80
Mathieu Pillard <m.pillard@liberation.fr>
2010-02-22 18:18:03 +0100

    Simplify the admin ; Will revisit this later

--- mptt_comments/admin.py
+++ mptt_comments/admin.py
@@ -10,15 +10,15 @@ class MpttCommentsAdmin(admin.ModelAdmin):
            {'fields': ('content_type', 'object_pk', 'parent', 'site')}
         ),
         (_('Content'),
-           {'fields': ('user', 'user_name', 'user_email', 'user_url', 'title', 'comment')}
+           {'fields': ('user', 'title', 'comment')}
         ),
         (_('Metadata'),
            {'fields': ('submit_date', 'ip_address', 'is_public', 'is_removed')}
         )
      )
 
-    list_display = ('name', 'content_type', 'object_pk', 'ip_address', 'comment', 'submit_date', 'is_public')
-    list_filter = ('submit_date', 'parent', 'site', 'is_public', 'is_removed')
+    list_display = ('title', 'user', 'content_type', 'object_pk', 'ip_address', 'submit_date', 'is_public', 'is_removed')
+    list_filter = ('submit_date', 'is_public', 'is_removed')
     date_hierarchy = 'submit_date'
     ordering = ('-submit_date',)
     search_fields = ('comment', 'user__username', 'user_name', 'user_email', 'user_url', 'ip_address')
@@ -26,5 +26,8 @@ class MpttCommentsAdmin(admin.ModelAdmin):
     def queryset(self, request):
         return MpttComment.objects.filter(parent__isnull=False)
 
-admin.site.unregister(Comment)
+try:
+	admin.site.unregister(Comment)
+except:
+	pass
 admin.site.register(MpttComment, MpttCommentsAdmin)
