b21a51ceaf312a600ea2b91be3f1de5d20083d4e
Justin Quick <jquick@INT1675.local>
2010-03-22 10:48:55 -0400

    filter/create instead of get_or_create to avoid race condition involving duplicate root nodes

--- mptt_comments/models.py
+++ mptt_comments/models.py
@@ -9,20 +9,23 @@ from django.contrib.comments.managers import CommentManager
 class MpttCommentManager(CommentManager):
     
     def get_root_comment(self, ctype, object_pk):
-        root_comment, uorc = self.model.objects.get_or_create(
-            parent=None,
-            content_type=ctype,
-            object_pk=str(object_pk),
-            defaults={
+        lookup = {
+            'parent':None,
+            'content_type':ctype,
+            'object_pk':str(object_pk),
+            'defaults':{
                 'comment': 'Root comment placeholder',
                 'user_name': 'Noname',
                 'user_email': 'no@user.no',
                 'user_url': '',
                 'submit_date': datetime.datetime.now(),
                 'site_id': settings.SITE_ID
-            })
-            
-        return root_comment
+            }
+        }
+        try:
+            return self.model.objects.filter(**lookup)[0]
+        except IndexError:
+            return self.model.objects.create(**lookup)
 
 class MpttComment(Comment):
     title = models.CharField(max_length=60)
