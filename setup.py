7dbf4ad0897f7eedf8e068636f7b12ce823e8c78
Justin Quick <justquick@gmail.com>
2010-09-01 14:20:17 -0400

    beter setuptools packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "django-mptt-comments",
-    version = '0.1.5',
-    url = 'http://bitbucket.org/fivethreeo/django-mptt-comments/',
-    author = 'fivethreeo',
+    version = '0.1.6',
+    url = 'http://github.com/justquick/django-mptt-comments/',
+    author = 'fivethreeo, justquick',
     description = 'Django Mptt Comments is a simple way to display threaded comments instead of the django contrib comments.',
-    packages = ['mptt_comments']
+    packages = find_packages(),
+    include_package_data=True,
 )
